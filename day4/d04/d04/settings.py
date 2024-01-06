db1db2981bf026dc28a91d49e50c7b2c0451654a
Antoine Labalette <labalette.antoine@gmail.com>
2024-01-06 20:18:40 +0100

    ok

--- day4/d04/d04/settings.py
+++ day4/d04/d04/settings.py
@@ -9,13 +9,15 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_ROOT = BASE_DIR / "static"
+
+LOG_FILE_PATH = os.path.join(BASE_DIR, "ex02", "logs.txt")
 
 
 # Quick-start development settings - unsuitable for production
@@ -35,6 +37,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "ex00.apps.Ex00Config",
     "ex01.apps.Ex01Config",
+    "ex02.apps.Ex02Config",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
