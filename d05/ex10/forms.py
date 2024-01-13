94587bf5b91024a638915cc69f56b73f7113e2b4
Antoine Labalette <labalette.antoine@gmail.com>
2024-01-13 14:04:36 +0100

    ok

--- d05/ex10/forms.py
+++ d05/ex10/forms.py
@@ -3,16 +3,17 @@ from .models import Movies, People, Planets
 
 
 class PeopleForm(forms.Form):
-    queryset = (
-        People.objects.all()
-        .values_list("gender", flat=True)
-        .distinct()
-        .exclude(gender="none")
-    )
-
     minReleaseDate = forms.DateField(required=True)
     maxReleaseDate = forms.DateField(required=True)
     minDiameter = forms.IntegerField(required=True)
-    gender = forms.ChoiceField(
-        choices=[(gender, gender) for gender in queryset], required=True
-    )  # Initialize with empty queryset
+    gender = forms.ChoiceField(required=True)
+
+    def __init__(self, *args, **kwargs):
+        super(PeopleForm, self).__init__(*args, **kwargs)
+        queryset = (
+            People.objects.all()
+            .values_list("gender", flat=True)
+            .distinct()
+            .exclude(gender="none")
+        )
+        self.fields["gender"].choices = [(gender, gender) for gender in queryset]
